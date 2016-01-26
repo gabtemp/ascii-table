316da8b5c47b32656b69cae22378d330c369c670
Gabriel Temporm Carneiro <gabtemp02@yahoo.com.br>
2016-01-25 22:17:35 -0200

    Fixing test

--- src/test/java/com/gcarneiro/table/TableImplTest.java
+++ src/test/java/com/gcarneiro/table/TableImplTest.java
@@ -30,7 +30,7 @@ public class TableImplTest {
         table.setRows(Collections.singletonList(row));
 
         String tableString = table.renderTable();
-        assertThat(tableString, is("+---------+\n| COLUMN  |\n+---------+\n| ROW     |\n+---------+\n"));
+        assertThat(tableString, is("+--------+\n| COLUMN |\n+--------+\n| ROW    |\n+--------+\n"));
     }
 
     @Test
@@ -48,7 +48,7 @@ public class TableImplTest {
         table.setRows(Collections.singletonList(row));
 
         String tableString = table.renderTable();
-        assertThat(tableString, is("+-------------+\n| COLUMN      |\n+-------------+\n| 24/01/2016  |\n+-------------+\n"));
+        assertThat(tableString, is("+------------+\n| COLUMN     |\n+------------+\n| 24/01/2016 |\n+------------+\n"));
     }
 
     private class TestRow {
